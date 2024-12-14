b93b1f24b000c540a0fe6f0444dc0577c5a98fb9
parisakh4 <126240144+parisakh4@users.noreply.github.com>
2024-12-14 13:24:23 +0100

    Improving charts, adding recommendations

--- CarbonEmission.py
+++ CarbonEmission.py
@@ -3,6 +3,26 @@ import os
 import pandas as pd
 import uuid
 from datetime import datetime
+import plotly.express as px
+
+#for colored bar for the active client in the bar chart
+def plot_colored_bar_chart(data, active_client_id, emission_type, title):
+    #Adding a column to distinguish active client
+    data['color'] = data['client_id'].apply(
+        lambda x: 'orange' if x == active_client_id else 'steelblue'
+    )
+
+    #Creatinh Plotly bar chart
+    fig = px.bar(
+        data,
+        x='short_client_id',
+        y=emission_type,
+        color='color',
+        color_discrete_map={'orange': 'orange', 'steelblue': 'steelblue'},
+        title=title,
+        labels={'short_client_id': 'Client ID', emission_type: f'{title} Emission'}
+    )
+    return fig
 
 
 #class for storing and managing client data
@@ -22,6 +42,8 @@ class CarbonFootprint:
         
     def calculate_waste_emission(self, waste_generate, waste_recycle):
         try:
+            #Converting waste_recycle to percentages if provided as whole number
+            waste_recycle = waste_recycle / 100 if waste_recycle > 1 else waste_recycle
             self.waste_emission = waste_generate * 12 * (0.57 - waste_recycle)
             return self.waste_emission
         except ValueError:
@@ -121,6 +143,18 @@ def main():
                 energy_benchmark= 3000 #put real value
                 if carbon_footprint.energy_emission > energy_benchmark:
                     st.warning(f"Your energy usage exceeds the benchmark of {energy_benchmark} kgCO2/year.")
+                    with st.expander("See recomendations for Improvements."):
+                        st.write("""Recommendations for reducing carbon emissions from energy usage in commercial buildings:<br><br> 
+                                 **Energy Management Systems:**<br> Implement effective energy management systems to monitor and control energy usage efficiently.<br><br> ​
+                                **HVAC Systems:**<br> Upgrade to energy-efficient HVAC systems, including the use of heat recovery, natural ventilation, ground source heat pumps, high-efficiency chillers, and variable frequency drives (VFD) for pumps.<br><br> ​
+                                **Lighting Systems:**<br> Use energy-efficient lighting solutions such as compact fluorescent bulbs, LED lights, and automated lighting controls.<br><br> ​
+                                **Building Envelope Improvements:**<br> Enhance building insulation, adopt cool roof technology, and use environmentally friendly construction materials to improve thermal performance and reduce energy consumption.<br><br> ​
+                                **Occupant Behavior:**<br> Train occupants, building managers, and tenants on energy efficiency practices and encourage behavior changes to reduce energy usage.<br><br> ​
+                                **Energy Monitoring:**<br> Install sensors, sub-meters, and CO2 sensors to optimize energy usage and improve energy efficiency.<br><br> ​
+                                **Renewable Energy:**<br> Integrate renewable energy sources such as solar panels and combined cooling, heating, and power (CCHP) systems to reduce reliance on fossil fuels. <br><br>​
+                                **Retrofits and Upgrades:**<br> Conduct energy audits and retrofits to identify and implement energy-saving measures, such as upgrading data centers and server rooms, and improving the energy efficiency of motors and on-site generation.<br><br> ​
+                                **Government Support and Incentives:**<br> Leverage government programs, incentives, and guidelines to support the implementation of energy-saving measures and promote the adoption of green building practices.<br><br> ​
+                                These recommendations aim to reduce energy consumption and, consequently, carbon emissions from commercial buildings. Sources:[Renewable and Sustainable Energy Reviews](https://www.sciencedirect.com/science/article/pii/S1364032119307531?casa_token=JVgGp10Cn1QAAAAA:4WsyKrseSoDtFCIBbjuDQuobv6m-edBWj0kFDvgm4JPbY8oWTBm9FddwkVYI4f3sm2EphfQBbks), [Energy and Buildings](https://www.sciencedirect.com/science/article/pii/S037877881930595X?casa_token=thtFaPllaYYAAAAA:Utb84qtEZV-PcYsPPpT5SnN56QQi2EK0yzD3tX8n9Qfl44pbRMysqsIysJxzjAxWZ2w72904kDw) ​ """, unsafe_allow_html=True)
                 else:
                     st.success(f"Your energy usage is within the benchmark of {energy_benchmark} kgCO2/year.")
             
@@ -150,6 +184,31 @@ def main():
                 waste_benchmark = 300  # Example benchmark in kgCO2
                 if carbon_footprint.waste_emission > waste_benchmark:
                         st.warning(f"Your waste management exceeds the benchmark of {waste_benchmark} kgCO2/year.")
+                        with st.expander("See Recommendations for Improvement"):
+                            st.write(
+                                    """Recommendations for Reducing Waste Emissions in Commercial Buildings To effectively reduce waste emissions in commercial buildings, the following inclusive recommendations are proposed:<br><br>
+                                    **Implement Separate Collection Models:**<br>
+                                    Adopt the dry-wet separate collection model within commercial buildings. Ensure that waste segregation bins are easily accessible and clearly labeled to facilitate the removal of dry recyclables and the composting of organic waste.<br><br>
+                                    **Promote Composting:**<br>
+                                    Establish on-site or off-site composting programs for organic waste generated in commercial buildings, such as food scraps from cafeterias and landscaping waste. Use the matured compost as a substitute fertilizer for landscaping or offer it to local agricultural projects.<br><br> ​
+                                    **Controlled Waste Disposal:**<br>
+                                    Ensure that non-recyclable and non-compostable waste is disposed of in controlled landfills. Implement waste audits to identify and minimize the amount of waste sent to landfills.<br><br>
+                                    **Support Organized Recycling Programs:**<br>
+                                    Develop and support organized recycling programs within commercial buildings. Provide training and resources to employees to encourage participation in recycling activities. Partner with local recycling facilities to ensure proper processing of recyclable materials.<br><br> ​
+                                    **Develop and Implement Green Building Certifications:**<br>
+                                    Pursue green building certifications such as LEED (Leadership in Energy and Environmental Design) or BREEAM (Building Research Establishment Environmental Assessment Method) that include waste management criteria. Implement practices that align with these certifications to reduce waste emissions.<br><br> ​
+                                    **Enhance Financial and Institutional Support:**<br>
+                                    Apply economic instruments such as waste disposal fees, recycling credits, and subsidies to incentivize better waste management practices within commercial buildings. ​ Ensure these instruments are fair, enforced, and adapted to the specific needs of commercial properties.<br><br> ​
+                                    Strengthen policies and guidelines for integrated waste management in commercial buildings and secure adequate funding for waste management services.
+                                    **Increase Public Awareness and Education:**<br>
+                                    Conduct awareness programs and training sessions for building occupants and staff to educate them on the importance of waste segregation, recycling, and composting. Promote environmental awareness and the benefits of sustainable waste management practices.<br><br> ​
+                                    **Encourage Public-Private Partnerships:**<br>
+                                    Foster public-private partnerships to enhance waste management services in commercial buildings. ​ Involve private sector companies in recycling and waste diversion activities, and ensure their participation is regulated and monitored.<br><br>
+                                    **Local Involvement and Customization:**<br>
+                                    Engage building management and occupants in the design and implementation of waste management systems. Tailor solutions to the specific needs and conditions of the commercial building to ensure effective and sustainable waste management practices.<br><br>
+                                    **Monitor and Evaluate:**<br>
+                                    Establish robust monitoring and evaluation mechanisms to track the effectiveness of waste management strategies and carbon emission reductions in commercial buildings. Use data to continuously improve practices and policies.<br><br>
+                                    By implementing these recommendations, commercial buildings can significantly reduce waste emissions, improve environmental quality, and promote sustainable business practices. Source:[Waste Manageent](https://www.sciencedirect.com/science/article/pii/S0956053X10002229?casa_token=4cmTS-biZmoAAAAA:jo_geg3FXEVFtM9NB3Sw__ay1SmBxeA8uoRFBvnWD-crXpu-1G0a1OcQpNCGgdEB2FhaJCGQdGg)""", unsafe_allow_html=True)
                 else:
                         st.success(f"Your waste management is within the benchmark.")
         
@@ -179,6 +238,36 @@ def main():
                 travel_benchmark = 10000
                 if carbon_footprint.travel_emission > travel_benchmark:
                     st.warning(f"Your travel emission exceeds the benchmark of {travel_benchmark} kgCO2/year.")
+                    with st.expander("See recommendations for improvement"):
+                        st.write("""Recommendations for reducing carbon emissions from business travels:<br><br>
+                                **Reduce the Need for Travel:**<br>
+                                Implement policies that prioritize virtual meetings over physical travel whenever possible. ​<br>
+                                Encourage employees to replace at least one face-to-face meeting with a virtual meeting to save costs and reduce carbon emissions. ​<br><br>
+                                **Change Travel Modes:**<br>
+                                Promote the use of less carbon-intensive travel modes, such as trains instead of flights or cars.<br> ​
+                                Provide incentives for employees to use public transportation or carpooling for business trips.<br><br>
+                                **Optimize Travel Policies:**<br>
+                                Reduce the class of air travel from business to economy to make flying less attractive and encourage alternative modes of travel.<br> ​
+                                Re-negotiate contracts with travel management companies to include sustainability criteria.<br><br>
+                                **Improve Meeting Management:**<br>
+                                Implement better meeting management practices to ensure that meetings are productive and time-efficient, reducing the need for extended travel.<br> ​
+                                Set realistic agendas and objectives for meetings to maximize their effectiveness.<br><br> ​
+                                **Benefit from Technology:**<br>
+                                Utilize new communication methods such as social media, smart phones, and collaborative tools to facilitate remote work and reduce the need for travel.<br><br ​
+                                Encourage the use of desktop video conferencing and instant messaging for quick and efficient communication.<br><br>
+                                **Address Organizational Culture and Behaviors:**<br>
+                                Foster a culture that values sustainability and supports the use of virtual communication tools.<br> ​
+                                Provide training and support to employees to help them become comfortable with using virtual meeting technologies.<br><br>
+                                **Public Accountability and Reporting:**<br>
+                                Participate in external reporting frameworks like the Carbon Reduction Commitment and the Carbon Disclosure Project to drive internal changes and maintain a good reputation with customers and stakeholders.<br> ​
+                                Set and monitor carbon budgets or targets for business travel to encourage responsible travel practices.<br><br> ​
+                                **Customer Engagement:**<br>
+                                Work with customers to manage expectations and promote the use of virtual meetings for supplier interactions.<br> ​
+                                Educate customers on the benefits of reducing carbon emissions from business travel and encourage their participation in sustainability initiatives.<br><br>
+                                **Support Work-Life Balance:**<br>
+                                Develop policies that support flexible working arrangements and reduce the need for frequent travel, improving employees' work-life balance.<br> ​
+                                Encourage home working and remote collaboration to minimize the necessity of physical presence.<br><br> ​
+                                By implementing these recommendations, organizations can effectively reduce carbon emissions from business travel while maintaining productivity and meeting business objectives. Source: [Transportation Research Part A: Policy and Practice](https://www.sciencedirect.com/science/article/pii/S096585641400202X?casa_token=I8YVgZJoUh4AAAAA:3Gy5FpCCnQA5fiJr9KODugw7n-MxPESsu_31tT5dDq6onkoewG9xNh4yPxtZ4bC4h0szEOKbKBU) ​ """, unsafe_allow_html=True)
                 else:
                     st.success("Your travel emission is within the range.")
 
@@ -216,27 +305,40 @@ def main():
     all_reports = all_reports.dropna(subset=["energy_emission", "waste_emission", "travel_emission"])
 
     all_reports['short_client_id'] = all_reports['client_id'].apply(lambda x: x[:8])
+    
+    #Ensure active client data is included if complete
+    active_client_id = st.session_state["active_client_id"]
+    if active_client_id in all_reports["client_id"].values:
+        active_client_data = all_reports[all_reports["client_id"] == active_client_id]
+        if active_client_data.dropna(subset=["energy_emission", "waste_emission", "travel_emission"]).empty:
+            # Remove active client row if incomplete
+            all_reports = all_reports[all_reports["client_id"] != active_client_id]
+
 
     if not all_reports.empty:
         # Bar chart for energy emissions
-        energy_chart_data = all_reports[["short_client_id", "energy_emission"]].set_index("short_client_id")
-        st.write("#### Energy Emissions Comparison")
-        st.bar_chart(energy_chart_data)
+        st.plotly_chart(
+        plot_colored_bar_chart(all_reports, st.session_state["active_client_id"], 
+                           "energy_emission", "Energy Emissions Comparison")
+        )
 
         # Bar chart for waste emissions
-        waste_chart_data = all_reports[["short_client_id", "waste_emission"]].set_index("short_client_id")
-        st.write("#### Waste Emissions Comparison")
-        st.bar_chart(waste_chart_data)
+        st.plotly_chart(
+        plot_colored_bar_chart(all_reports, st.session_state["active_client_id"], 
+                           "waste_emission", "Waste Emissions Comparison")
+        )
 
         # Bar chart for travel emissions
-        travel_chart_data = all_reports[["short_client_id", "travel_emission"]].set_index("short_client_id")
-        st.write("#### Travel Emissions Comparison")
-        st.bar_chart(travel_chart_data)
+        st.plotly_chart(
+        plot_colored_bar_chart(all_reports, st.session_state["active_client_id"], 
+                           "travel_emission", "Travel Emissions Comparison")
+        )
         
         #Total emissions comparison
-        total_chart_data = all_reports[["short_client_id", "total_emission"]].set_index("short_client_id")
-        st.write("#### Total Emissions Comparison")
-        st.bar_chart(total_chart_data)
+        st.plotly_chart(
+        plot_colored_bar_chart(all_reports, st.session_state["active_client_id"], 
+                           "total_emission", "Total Emissions Comparison")
+        )
     else:
         st.warning("No data available for comparison. Save at least one report first!")
     
